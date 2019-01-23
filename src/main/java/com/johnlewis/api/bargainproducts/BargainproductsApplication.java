2baa50fcc9fb9db6de94a73954bb9e9440157e42
Ali Dardagan <ali.dardagan@rightmove.co.uk>
2019-01-23 21:35:11 +0000

    Initial spring boot project commit

---
+++ src/main/java/com/johnlewis/api/bargainproducts/BargainproductsApplication.java
@@ -0,0 +1,14 @@
+package com.johnlewis.api.bargainproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BargainproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BargainproductsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/johnlewis/api/bargainproducts/BargainproductsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.johnlewis.api.bargainproducts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BargainproductsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
